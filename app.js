82f6c34b48e57e5d9a93aaf6c4eec863f1831962
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-02 03:17:00 -0400

    added node file module to read and store file content

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // Requiring the node http module
-var http = require('http');
+// Requiring the node file module
+var http = require('http'),
+		fs = require('fs');
 
 // Storing the content type object in contentTypeHeader var
-var contentTypeHeader = {'Content-Type': 'text/plain'};
+var contentTypeHeader = {'Content-Type': 'text/html'};
 
 // Creating the web server
 http.createServer(function(req, res) {
 	res.writeHead(200, contentTypeHeader);
-	res.end("Hello!\n");
+	// Synchronously reading the file content from the specified loction and storing the content in htmlContent var
+	var htmlContent = fs.readFileSync(__dirname + '/HTMLmockups/weather.html');
+	// When the response ends/finish the htmlContent is sent to the server
+	res.end(htmlContent);
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running on localhost:3000');
