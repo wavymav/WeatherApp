56e9644a099cf1080bd72f94591d3c768dc8d2b8
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-02 19:54:15 -0400

    added eventConfig file for the event string values to prevent typos

---
+++ eventConfig.js
@@ -0,0 +1,10 @@
+// This eventConfig file is ment to prevent me from using magic strings for events
+// Very usefull for the EventEmitter
+// Helps prevent typos for events!
+ module.exports = {
+	 events: {
+		 DATA: 'data',
+		 END: 'end',
+		 ERROR: 'error'
+	 }
+ };
--- weather.js
+++ weather.js
@@ -1,10 +1,13 @@
 // Requiring the EventEmitter from the events node module
 // Requiring the util node module
 // Requiring the http node module
+// Requiring apiKey.js
+// Requiring eventConfig.js
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var http = require('http');
 var apiKey = require('./apiKey');
+var eventConfig = require('./eventConfig').events;
 
 // Weather function constuctor object
 function Weather(cityName) {
