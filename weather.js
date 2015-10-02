16ce443be4734ad2a3a0acafbc283134e77b8d9d
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-02 19:34:57 -0400

    added the apiKey & created the request object for OWM API

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 // Requiring the EventEmitter from the events node module
 // Requiring the util node module
 // Requiring the http node module
-var EventEmitter = require("events").EventEmitter;
-var util = require("util");
-var http = require("http");
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var http = require('http');
+var apiKey = require('./apiKey');
 
 // Weather function constuctor object
 function Weather(cityName) {
@@ -11,6 +12,11 @@ function Weather(cityName) {
 	// Inherits the constructor
 	EventEmitter.call(this);
 	weatherEmitter = this;
+
+		// Connecting to the OpenWeatherMap API
+		var req = http.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',us&units=imperial=' + apiKey, function(res){
+
+		});
 }
 
 // Weather constructor object gains access to the EventEmitter's prototype chain
