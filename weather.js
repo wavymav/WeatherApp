5ce5cbe23d69400fff0853076576420bd64aee5b
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-02 21:22:37 -0400

    tested connection to API

--- weather.js
+++ weather.js
@@ -18,10 +18,27 @@ function Weather(cityName) {
 
 		// Connecting to the OpenWeatherMap API
 		var req = http.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',us&units=imperial=' + apiKey, function(res){
+			// logging the response object status codes
+			console.log(res.statusCode);
+			console.log(http.STATUS_CODES[res.statusCode]);
 
+			// If response object status code is not 200 (OK)
+			if (res.statusCode !== 200) {
+				// Abort Request
+				req.abort();
+				console.log('request aborted!');
+			}
+
+		});
+
+		// Handling error events on the request object
+		req.on(eventConfig.ERROR, function(error) {
+			console.error('There was an error: ' + error.message);
 		});
 }
 
 // Weather constructor object gains access to the EventEmitter's prototype chain
 // Inherits the prototype chain
 util.inherits(Weather, EventEmitter);
+
+var currentWeather = new Weather('');
