a6001dd2149380182560c10ae0ee01b409d7d76b
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-03 00:54:47 -0400

    both homeSerch root route & weatherReport route working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var routes = require('./routes');
 
 // Creating the web server
 http.createServer(function(req, res) {
-	routes.home(req, res);
+	routes.homeSearch(req, res);
+	routes.weatherReport(req, res);
 }).listen(3000, '127.0.0.1');
 console.log('Server running on localhost:3000');
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 // Requiring the node file module
 var fs = require('fs');
+var Weather = require('./weather');
 
 var exports = module.exports = {};
 
@@ -7,11 +8,25 @@ var exports = module.exports = {};
 var contentTypeHeader = {'Content-Type': 'text/html'};
 
 // setting up the home route
-exports.home = function(req, res) {
+exports.homeSearch = function(req, res) {
+	// If the req.url is '/' then you're in the root dir
 	if (req.url === '/') {
 		res.writeHead(200, contentTypeHeader);
 		// Synchronously reading the file content from the specified loction and storing the content in htmlContent var
 		var htmlContent = fs.readFileSync(__dirname + '/HTMLmockups/search.html');
+		// When the response ends/finish the htmlContent is sent to the server
+		res.end(htmlContent);
+	}
+};
+
+exports.weatherReport = function(req, res) {
+	// Replacing the req.url root('/') with an empty string
+	var cityName = req.url.replace('/', '');
+
+	if (cityName.length > 0) {
+		res.writeHead(200, contentTypeHeader);
+		// Synchronously reading the file content from the specified loction and storing the content in htmlContent var
+		var htmlContent = fs.readFileSync(__dirname + '/HTMLmockups/weather.html');
 		// When the response ends/finish the htmlContent is sent to the server
 		res.end(htmlContent);
 	}
