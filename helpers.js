8a5820efc0430013c207d9ae7361adf6b5576785
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-04 16:38:52 -0400

    fixed bug in helpers.js for the removeDecimalAndDigits()

--- helpers.js
+++ helpers.js
@@ -9,11 +9,19 @@ var convertToString =  function(number) {
 
 // removes the deciaml and digits from the temperature string passed in
 exports.removeDecimalAndDigits = function(number) {
-	var	string = convertToString(number),
+	var	string = convertToString(number);
 	// finds the index of the deciaml or ('.' character) in the string
-			endIndex = string.indexOf('.'),
-	// uses the number value in endIndex as the endpoint of the slice method
-			value = string.slice(0, endIndex);
+	var endIndex = string.indexOf('.');
+	// value decloration
+	var value;
+
+	// if '.' does not exits in the string do not call slice method
+	if (endIndex === -1) {
+		value = string;
+	} else {
+		// uses the number value in endIndex as the endpoint of the slice method
+		value = string.slice(0, endIndex);
+	}
 
 	return value;
 };
